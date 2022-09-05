582ee68b67631058497b2b35c1d3be894e82a5c8
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2022-09-05 13:19:18 -0600

    Iniciando proyecto

---
+++ crud-oracle/src/main/java/com/fernando/crudoracle/CrudOracleApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.crudoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOracleApplication.class, args);
+	}
+
+}
---
+++ crud-oracle/src/test/java/com/fernando/crudoracle/CrudOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.crudoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
