a4cd292ffb5eada21305a9f5578898c14e5a532a
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2022-09-21 09:30:32 -0600

    Correccion

--- README.md
+++
@@ -1,2 +0,0 @@
-# CRUD-Spring-boot-Mysql
-CRUD utilizando Spring boot y base de datos Mysql
--- crud-oracle/src/main/java/com/app/CRUDOracle/repo/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.app.CRUDOracle.repo;
-
-import com.app.CRUDOracle.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
-
-}
